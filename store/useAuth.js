1c4d20b0c36660850f2159b19dde8408fdb16e55
beso <besogigashvili818@gmail.com>
2021-12-12 11:22:04 +0400

    front

---
+++ store/useAuth.js
@@ -0,0 +1,12 @@
+// import AuthContext from "./authContext";
+// import { useContext } from "react";
+
+// const useAuth = () => {
+//   const context = useContext(AuthContext);
+//   if (!context) {
+//     throw new Error("Component is not in the provider");
+//   }
+//   return context;
+// };
+
+// export default useAuth;
